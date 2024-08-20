944626910d7cc273a1f86d98e1200bbc37d4109a
Kaloian <kalioanpencev8@gmail.com>
2024-08-20 21:46:38 +0300

    landing page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import "../styles/Landing.css";
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default Landing;
