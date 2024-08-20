132453ebdb0478760dee9dbf22b1c2ab4ee397c5
Kaloian <kalioanpencev8@gmail.com>
2024-08-20 22:20:37 +0300

    updated navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <section className="navbar">
+      <div className="left">
+        <h1>Hello</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-//import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import "../styles/Landing.css";
 
 const Landing = () => {
   return (
     <>
-      
+      <Navbar/>
       <HeroSection />
     </>
   );
