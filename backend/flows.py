2cd5588a92f8660d1ad02607195fc2e7ce2d9bd1
Kaloian <kalioanpencev8@gmail.com>
2024-09-28 16:48:26 +0300

    fixed routes

--- backend/flows.py
+++ backend/flows.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, request, jsonify, session
+from flask_login import login_required
+
 
 flows = Blueprint('flows', __name__)
 
+
 @flows.route('/api/home', methods=['POST'])
+@login_required
 def home():
+
     return jsonify({'message': 'Welcome!', 'redirectTo': '/home'}), 200
 
